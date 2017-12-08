dff50a0410943827f2d146337c04fcae8b436e9f
owner <olga.penina@emc.com>
2017-12-08 16:55:52 -0500

    Groomed the Critter

--- src/components/Critter.js
+++ src/components/Critter.js
@@ -1,17 +1,17 @@
-export default function Critter(lName, money){
-    this.name = lName;
+export default function Critter(name, money) {
+    this.name = name;
     this.money = money;
-    this.talk = function(){
+    this.talk = function () {
         var msg = "Hi! my name is " + this.name;
         msg += " and I own  " + this.money + " coins";
         alert(msg);
     }
 
-    this.increase = function(amount){
-       this.money = this.money + amount
+    this.increase = function (amount) {
+        this.money += amount;
     }
 
-    this.decrease = function(amount){
-        this.money = this.money - amount
+    this.decrease = function (amount) {
+        this.money -= amount;
     }
 }
